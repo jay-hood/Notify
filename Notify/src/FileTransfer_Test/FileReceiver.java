56dd2ab8ba267ce35e289286cb7e7d900b700aa7
Jay Hood <hood.jay@gmail.com>
2018-01-29 19:06:15 -0500

    file transfer test package and files working

--- Notify/src/FileTransfer_Test/FileReceiver.java
+++ Notify/src/FileTransfer_Test/FileReceiver.java
@@ -1,4 +1,23 @@
 package FileTransfer_Test;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class FileReceiver {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(9002);
+        Socket socket = serverSocket.accept();
+
+        //DataOutputStream out = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+        DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        byte[] bytes = new byte[1024];
+
+        in.read(bytes);
+        System.out.println(bytes);
+
+        FileOutputStream fos = new FileOutputStream("C:\\users\\hoodj\\Desktop\\FileTest\\test.txt");
+        fos.write(bytes);
+        in.close();
+    }
 }
--- Notify/src/FileTransfer_Test/FileSender.java
+++ Notify/src/FileTransfer_Test/FileSender.java
@@ -1,4 +1,37 @@
 package FileTransfer_Test;
 
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class FileSender {
+    public static void main(String[] args) {
+
+        String fileLocation = "C:\\users\\hoodj\\Desktop\\test.txt";
+        File file = new File(fileLocation);
+        //long fileSize = file.length();
+        byte [] array = new byte[8192];
+
+        //Need to read the documentation on input and output streams
+        int port = 9002;
+        try {
+            InetAddress host = InetAddress.getLocalHost();
+            Socket socket = new Socket(host, port);
+            FileInputStream input = new FileInputStream(file);
+            OutputStream output = socket.getOutputStream();
+            int count;
+            while((count = input.read(array)) > 0){
+                output.write(array, 0, count);
+            }
+            input.close();
+            output.close();
+            socket.close();
+
+        }
+        catch(IOException iex){
+            iex.printStackTrace();
+        }
+
+
+    }
 }
