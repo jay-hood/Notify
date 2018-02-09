e57c4908d11cae3a2adeb997bebf90eef43ef981
Jay Hood <hood.jay@gmail.com>
2018-02-08 21:56:37 -0500

    started on GUI

--- TestProject/src/sample/Controller.java
+++ TestProject/src/sample/Controller.java
@@ -1,4 +1,44 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+    private String userMessage;
+    private Boolean connectionStatus = true;
+
+    @FXML
+    private TextArea userEntry;
+
+    @FXML
+    private TextArea receivedMessages;
+
+
+    /*
+    I think a good idea would be to test the functionality by passing the text from the user entry box
+    to the received messages box. Just to make sure everything works alright and formats properly before attempting to
+    add genuine communications.
+     */
+    public void submitButtonClicked(){
+        userMessage = userEntry.getText().replaceAll("\n", System.getProperty("line.separator"));
+        userEntry.clear();
+    }
+
+
+    public String getUserMessage(){
+        return userMessage;
+    }
+
+    public void setReceivedMessages(String message){
+        receivedMessages.setText(message);
+    }
+
+    public void connectionClosed(){
+        connectionStatus = false;
+        userEntry.setText("Connection closed.");
+    }
+
+    public boolean getConnectionStatus(){
+        return connectionStatus;
+    }
 }
--- TestProject/src/sample/Main.java
+++ TestProject/src/sample/Main.java
@@ -1,23 +1,88 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main extends Application {
+
+    private Socket socket;
+    private int port = 9000;
+    private InetAddress host;
+    private Scanner scanner;
+    private Scanner receiver;
+    private PrintWriter printWriter;
+    private String message = "";
+    private boolean connected = true;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Controller controller = new Controller();
+        loader.setController(controller);
+
+        try {
+            host = InetAddress.getLocalHost();
+            socket = new Socket(host, port);
+            receiver = new Scanner(socket.getInputStream());
+            printWriter = new PrintWriter(socket.getOutputStream(), true);
+            InboundMessageHandler handler = new InboundMessageHandler(receiver, controller);
+            Thread thread = new Thread(handler);
+            thread.start();
+        }
+        catch(IOException iex){
+            iex.printStackTrace();
+        }
+        /*
+        This while loop was originally intended for use with the console.
+        I need to make a new method that will integrate with the GUI.
+        */
+
+        while(controller.getConnectionStatus()){
+            printWriter.println(controller.getUserMessage());
+        }
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
+
+    private class InboundMessageHandler implements Runnable {
+
+        private String message = "";
+        private Scanner receiver;
+        Controller controller;
+
+        public InboundMessageHandler(Scanner receiver, Controller controller){
+            this.controller = controller;
+            this.receiver = receiver;
+        }
+
+        @Override
+        public void run(){
+            while(controller.getConnectionStatus()){
+                message = receiver.nextLine();
+                controller.setReceivedMessages(message);
+
+            }
+        }
+    }
 }
+
+
