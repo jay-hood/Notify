029cd1f63773c6cb487e0d820e16408a3cbf9a38
Jay Hood <hood.jay@gmail.com>
2018-02-13 11:45:06 -0500

    added new .xml file for username and ip address and added functionality to classes

--- TestProject/src/sample/Controller.java
+++ TestProject/src/sample/Controller.java
@@ -1,11 +1,28 @@
 package sample;
 
+//import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Controller {
     private String userMessage;
     private Boolean connectionStatus = true;
+    private Socket socket;
+    private int port = 9000;
+    private InetAddress host;
+    private Scanner receiver;
+    private PrintWriter printWriter;
 
     @FXML
     private TextArea userEntry;
@@ -14,6 +31,20 @@ public class Controller {
     private TextArea receivedMessages;
 
 
+    public void initConnection(){
+        try {
+            host = InetAddress.getLocalHost();
+            socket = new Socket(host, port);
+            receiver = new Scanner(socket.getInputStream());
+            printWriter = new PrintWriter(socket.getOutputStream(), true);
+            InboundMessageHandler handler = new InboundMessageHandler(receiver);
+            Thread thread = new Thread(handler);
+            thread.start();
+        }
+        catch(IOException iex){
+            iex.printStackTrace();
+        }
+    }
     /*
     I think a good idea would be to test the functionality by passing the text from the user entry box
     to the received messages box. Just to make sure everything works alright and formats properly before attempting to
@@ -22,9 +53,24 @@ public class Controller {
     public void submitButtonClicked(){
         userMessage = userEntry.getText().replaceAll("\n", System.getProperty("line.separator"));
         userEntry.clear();
+        printWriter.println(userMessage);
     }
 
+    public void connectionClicked(){
+        try{
+            FXMLLoader loadConnectionWindow = new FXMLLoader(getClass().getResource("connection.fxml"));
+            Parent root1 = loadConnectionWindow.load();
+            Stage stage = new Stage();
+            stage.setTitle("Connection Details");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        }
+        catch(IOException iex){
+            iex.printStackTrace();
+        }
+    }
 
+    /*
     public String getUserMessage(){
         return userMessage;
     }
@@ -33,12 +79,33 @@ public class Controller {
         receivedMessages.setText(message);
     }
 
+
+
+    public boolean getConnectionStatus(){
+        return connectionStatus;
+    }
+    */
+
     public void connectionClosed(){
         connectionStatus = false;
         userEntry.setText("Connection closed.");
     }
 
-    public boolean getConnectionStatus(){
-        return connectionStatus;
+    private class InboundMessageHandler implements Runnable {
+
+        private String message = "";
+        private Scanner receiver;
+
+        public InboundMessageHandler(Scanner receiver){
+            this.receiver = receiver;
+        }
+
+        @Override
+        public void run(){
+            while(connectionStatus){
+                message = receiver.nextLine();
+                receivedMessages.setText(message);
+            }
+        }
     }
 }
--- TestProject/src/sample/Main.java
+++ TestProject/src/sample/Main.java
@@ -1,60 +1,28 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.util.Scanner;
 
 public class Main extends Application {
 
-    private Socket socket;
-    private int port = 9000;
-    private InetAddress host;
-    private Scanner scanner;
-    private Scanner receiver;
-    private PrintWriter printWriter;
-    private String message = "";
-    private boolean connected = true;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Notify!");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
-        Controller controller = new Controller();
-        loader.setController(controller);
-
-        try {
-            host = InetAddress.getLocalHost();
-            socket = new Socket(host, port);
-            receiver = new Scanner(socket.getInputStream());
-            printWriter = new PrintWriter(socket.getOutputStream(), true);
-            InboundMessageHandler handler = new InboundMessageHandler(receiver, controller);
-            Thread thread = new Thread(handler);
-            thread.start();
-        }
-        catch(IOException iex){
-            iex.printStackTrace();
-        }
         /*
-        This while loop was originally intended for use with the console.
-        I need to make a new method that will integrate with the GUI.
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Controller controller = loader.getController();
+        controller.connectionClicked();
+        controller.initConnection();
         */
 
-        while(controller.getConnectionStatus()){
-            printWriter.println(controller.getUserMessage());
-        }
     }
 
 
@@ -63,26 +31,6 @@ public class Main extends Application {
         launch(args);
     }
 
-    private class InboundMessageHandler implements Runnable {
-
-        private String message = "";
-        private Scanner receiver;
-        Controller controller;
-
-        public InboundMessageHandler(Scanner receiver, Controller controller){
-            this.controller = controller;
-            this.receiver = receiver;
-        }
-
-        @Override
-        public void run(){
-            while(controller.getConnectionStatus()){
-                message = receiver.nextLine();
-                controller.setReceivedMessages(message);
-
-            }
-        }
-    }
 }
 
 
